8eea2ca75a047bac9a2316bfdf695284e30d69c0
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-04-27 18:56:11 -0500

    fix: suppress warning.

--- src/app/shared/services/base.service.spec.ts
+++ src/app/shared/services/base.service.spec.ts
@@ -1,7 +1,11 @@
 import { BaseService } from './base.service';
+import * as http from "node:http";
+import {HttpClient} from "@angular/common/http";
+
 
 describe('BaseService', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new BaseService()).toBeTruthy();
   });
 });
